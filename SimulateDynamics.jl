41062bb850fa52690c36380b8d4fcaf504c7e34c
JohnETobin <johntobin788@gmail.com>
2022-06-01 10:30:47 -0500

    Update SimulateDynamics.jl

--- SimulateDynamics.jl
+++ SimulateDynamics.jl
@@ -1,4 +1,18 @@
-using Pkg
-Pkg.add("MatrixNetworks")
 using MatrixNetworks
+using LinearAlgebra
+
+function linear_model(t, x, A, epsilon)
+    (epsilon*A-np.eye(len(A))).dot(x)
+end
+
+function SIS_model(t, x, A, gamma, beta)
+  n = np.size(A, axis=0)
+  -gamma*x + beta*np.multiply((np.ones(n)-x), A.dot(x))
+end
+
+function SI_model(t, x, A, beta)
+  n = np.size(A, axis=0)
+  beta*np.multiply((np.ones(n)-x), A.dot(x))
+end
+
 
