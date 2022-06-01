4524486a60ff57c0080866e2756875f5aace2a11
dtlaurianti <dtlaurianti@gmail.com>
2022-06-01 10:44:06 -0500

    Update SimulateDynamics.jl
    Converted 2 functions to Julia

--- SimulateDynamics.jl
+++ SimulateDynamics.jl
@@ -1,13 +1,13 @@
 using MatrixNetworks
 using LinearAlgebra
 
-function linear_model(t, x, A, epsilon)
-    (epsilon*A-np.eye(len(A))).dot(x)
+function linear_model(t, x, A, ϵ)
+  (ϵ*A-I)⋅x
 end
 
-function SIS_model(t, x, A, gamma, beta)
-  n = np.size(A, axis=0)
-  -gamma*x + beta*np.multiply((np.ones(n)-x), A.dot(x))
+function SIS_model(t, x, A, γ, β)
+  nrow = size(A, 1)
+  -γ*x + β*[(ones(n)-x).*(A⋅x)]
 end
 
 function SI_model(t, x, A, beta)
