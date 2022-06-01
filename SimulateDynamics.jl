21564ce9822cdfee7b6c56d4fd7f25086f856d3d
dtlaurianti <dtlaurianti@gmail.com>
2022-06-01 11:04:11 -0500

    Update SimulateDynamics.jl
    : and end

--- SimulateDynamics.jl
+++ SimulateDynamics.jl
@@ -21,7 +21,7 @@ function kuramoto_model(t, x, A, omega=None, K=1)
     for i in 1:n
         S = sin(x[i]*ones(n).-(x))
         dxdt += K*(A[i,:].*(S))
-    end 
+    end
     dxdt
 end
 
@@ -30,7 +30,7 @@ function LotkaVolterra_model(t, x, A, omega)
   omega*x + (x.*(A⋅(x)))
 end
 
-function linear_opinions(t, x, A, c=1):
+function linear_opinions(t, x, A, c=1)
   #=
   Simplest model of linear opinion dynamics; c = constant, for now
   =#
@@ -38,8 +38,9 @@ function linear_opinions(t, x, A, c=1):
   Din  = diag(sum(A, 2))            # compute indegree matrix
   L =  Dout + Din - A - transpose(A)  # compute graph laplacian
   return -c*L⋅x
+end
 
-function nonlinear_opinions(t, x, A, d=0.1, u=1, b=0):
+function nonlinear_opinions(t, x, A, d=0.1, u=1, b=0)
   #=
   An instance of the model in https://arxiv.org/abs/2009.04332
   Parameters:
@@ -50,3 +51,4 @@ function nonlinear_opinions(t, x, A, d=0.1, u=1, b=0):
   =#
   n = size(A, 1)
   return - d*x + u*tanh(A⋅x)) + b*ones(n)
+end
