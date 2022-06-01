80b97923325e6d6b4b8d736e3c1b2e7c40e9829a
dtlaurianti <dtlaurianti@gmail.com>
2022-06-01 11:01:21 -0500

    Update SimulateDynamics.jl
    Converted linear and nonlinear opinion functions

--- SimulateDynamics.jl
+++ SimulateDynamics.jl
@@ -12,7 +12,7 @@ end
 
 function SI_model(t, x, A, β)
   n = size(A, 1)
-  beta*((ones(n)-x).*(A⋅x))
+  β*((ones(n)-x).*(A⋅x))
 end
 
 def kuramoto_model(t, x, A, omega=None, K=1):
@@ -27,23 +27,23 @@ def LotkaVolterra_model(t, x, A, omega):
   n = np.size(A, axis=0)
   return omega*x + np.multiply(x, A.dot(x))
 
-def linear_opinions(t, x, A, c=1):
-  '''
+function linear_opinions(t, x, A, c=1):
+  #=
   Simplest model of linear opinion dynamics; c = constant, for now
-  '''
-  Dout = np.diag(np.sum(A, axis=0))            # compute outdegree matrix
-  Din  = np.diag(np.sum(A, axis=1))            # compute indegree matrix
-  L =  Dout + Din - A - np.matrix.transpose(A)  # compute graph laplacian
-  return -c*L.dot(x)
+  =#
+  Dout = diag(sum(A, 1))            # compute outdegree matrix
+  Din  = diag(sum(A, 2))            # compute indegree matrix
+  L =  Dout + Din - A - transpose(A)  # compute graph laplacian
+  return -c*L⋅x
 
-def nonlinear_opinions(t, x, A, d=0.1, u=1, b=0):
-  '''
+function nonlinear_opinions(t, x, A, d=0.1, u=1, b=0):
+  #=
   An instance of the model in https://arxiv.org/abs/2009.04332
   Parameters:
       d = resistance,
       u = attention to social influence
       b = bias
   For now, d, u, b are constants, but we can eventually vary by individual
-  '''
-  n = np.size(A, axis=0)
-  return - d*x + u*np.tanh(A.dot(x)) + b*np.ones(n)
+  =#
+  n = size(A, 1)
+  return - d*x + u*tanh(A⋅x)) + b*ones(n)
