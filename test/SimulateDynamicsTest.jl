62ae17e7c7a0607a6e33591c5518dea5bab78a7b
dtlaurianti <dtlaurianti@gmail.com>
2022-06-03 15:25:19 -0500

    Plotting Functionality

--- test/SimulateDynamicsTest.jl
+++ test/SimulateDynamicsTest.jl
@@ -4,8 +4,14 @@ LG = line_graph(5)
 CG = cycle_graph(5)
 GG = grid_graph(4; directed=true)
 
-x = [1, 1, 1, 1, 1]
-sol1 = simulateODEonGraph(LG, x)
+x = [1, 1, 1, 1, 0]
+sol1 = simulateODEonGraph(CG, x)
+plt1 = plot(5,xlim=(0,1000),ylim=(0,1),title="line_graph linear_model")
+anim1 = @animate for i=1:1000
+    push!(plt1,sol1[i])
+end every 10
+display(gif(anim1))
+
 sol2 = simulateODEonGraph(LG, x, γ=0.1, β=0.1, dynamical_function=SIS_model)
 sol3 = simulateODEonGraph(LG, x, β=0.1, dynamical_function=SI_model)
 sol4 = simulateODEonGraph(LG, x, dynamical_function=kuramoto_model)
