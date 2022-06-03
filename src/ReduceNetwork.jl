67cff53894cdbbd916b9073ab08bc1ceb2c20afb
JohnETobin <johntobin788@gmail.com>
2022-06-03 15:04:58 -0500

    Adding to ReduceNetwork

--- src/ReduceNetwork.jl
+++ src/ReduceNetwork.jl
@@ -29,7 +29,7 @@ function compressAdjacencyMatrix(A, partition)
     numGroups = length(set(partition.values()))
     groupSizes = getSupernodeSizes(partition)
     K = zeros((length(groupSizes),length(A)))
-    R = zeros((len(A),len(groupSizes)))
+    R = zeros((length(A),length(groupSizes)))
     for i in 1:length(A)
         R[i,partition[i]] = 1
         K[partition[i],i] = 1/groupSizes[partition[i]]
@@ -39,20 +39,155 @@ end
 
 ######## Partitioning ########
 function generateRandomPartitions(originalSize, reducedSize, numPartitions)
-    partitionList = list()
-    for index in range(numPartitions):
-        partitionAccepted = False
-        while not partitionAccepted:
+    partitionList = []
+    for index in 1:numPartitions
+        partitionAccepted = false
+        while !partitionAccepted
             partition = dict()
-            labels = random.choices(range(reducedSize), k=originalSize)
-            for node in range(originalSize)
+            #labels = random.choices(range(reducedSize), k=originalSize)
+            labels = []
+            for i in 1:originalSize
+                push!(labels, sample(1:reducedSize))
+            end
+            for node in 1:originalSize
                 partition[node] = labels[node]
             end
-            if len(set(labels)) == reducedSize:
-                partitionAccepted = True
+            if length(set(labels)) == reducedSize:
+                partitionAccepted = true
             end
         end
-        partitionList.append(partition)
+        push!(partitionList, partition)
     end 
     return partitionList
+end
+
+function spectralClustering(A, reducedSize)
+    l, X = eigvals(diag(A))
+    #Figure out how to do this
+    labels = k_means(X[:,:reducedSize], reducedSize, init='k-means++')[1]
+    return {i:labels[i] for i in range(len(A))}
+end
+
+function mEEP(A, reducedSize)
+    return print("under dev")
+end
+
+# works only for undirected networks as of now.
+function agglomerationReduction(A, reducedSize)
+    partition = dict()
+    n = size(A, 1)
+    for nodeId in 1:n
+        partition[nodeId] = nodeId
+    end
+    k = sum(A, 1)
+    m = sum(k)
+    Q = -sum(k.^2)./m.^2
+    while length(set(values(partition))) > reducedSize
+        partition, Q = greedyMerge(A, Q, partition, k)
+        println(Q)
+    end
+    cleanedPartition = dict()
+    items = []
+    for i in partition
+        push!(items, i)
+    end
+    for node, group in items
+        try
+            partition[node] = cleanedPartition[group]
+        catch
+            try
+                cleanedPartition[group] = max(values(cleanedPartition)) + 1
+            catch
+                cleanedPartition[group] = 0
+            end
+            partition[node] = cleanedPartition[group]
+        end
+    end
+    return partition
+ end
+
+#not finished
+function greedyMerge(A, Q, partition, k)
+    # iterate over unique groups
+    m = sum(k)
+    groupIds = list(set(values(partition)))
+    numGroups = length(groupIds)
+    maxQ = -Inf
+    for groupIndex1 in 1:numGroups
+        for groupIndex2 in 1:groupIndex1
+            groupId1 = groupIds[groupIndex1]
+            groupId2 = groupIds[groupIndex2]
+
+            group1 = [nodeId for nodeId, group in partition.items() if group == groupId1]
+            group2 = [nodeId for nodeId, group in partition.items() if group == groupId2]
+
+            eUV = 0
+            aU = 0
+            aV = 0
+            for i in group1:
+                for j in group2:
+                    try:
+                        eUV += A[i,j]/m # because directed edge list
+                    except:
+                        pass
+            for i in group1:
+                aU += k[i]/m
+
+            for i in group2:
+                aV += k[i]/m
+
+            newQ = Q + 2*(eUV - aU*aV)
+
+            if newQ > maxQ:
+                maxQ = newQ
+                maxGroupId1 = groupId1
+                maxGroupId2 = groupId2
+    newPartition = dict(partition)
+    for node in list(newPartition.keys()):
+        if newPartition[node] == maxGroupId2:
+            newPartition[node] = maxGroupId1
+    return newPartition, maxQ
+end
+
+#not finished
+# WARNING: not super efficient at the moment!
+function exhaustivePartition(n):
+    '''
+    input: number of nodes n
+    output: dictionary of all possible partitions
+    '''
+    allPartitions = {}
+    nodeIds = list(range(n))
+
+    # compute all integer partitions
+    for index, part in enumerate(partitionNodes(nodeIds),1):
+        # print(index, part)
+
+        partition = dict()
+        for supernodeId, subnodeIds in enumerate(part):
+            for nodeId in subnodeIds:
+                partition[nodeId] = supernodeId
+
+        allPartitions[index] = partition
+
+    return allPartitions
+end
+
+function partitionNodes(nodeIds)
+    #=
+    input:  list of nodeIds
+    output: list of all possible partitions
+    =#
+    if len(nodeIds) == 1:
+        #TODO: determine how to get python-like yield functionality in Julia 
+        yield [ nodeIds ]
+        return
+    end
+    first = nodeIds[0]
+    for smaller in partitionNodes(nodeIds[1:]):
+        # insert first in each of the subpartition's subsets
+        for k, subset in enumerate(smaller)
+            yield smaller[:k] + [[ first ] + subset] + smaller[k+1:]
+        # put first in its own subset
+        yield [[ first ]] + smaller
 end
