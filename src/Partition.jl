0b9ad17b975d3c67597a461cfd293c3bdf58452e
dtlaurianti <dtlaurianti@gmail.com>
2022-06-06 14:46:12 -0500

    Update Partition.jl
    converting

--- src/Partition.jl
+++ src/Partition.jl
@@ -1,21 +1,22 @@
 using SparseArrays
 using Clustering
+using ResumableFunctions
 
 # generate numPartitions random partitions mapping a network of size originalSize
 # to a network of size reducedSize
 function generateRandomPartitions(originalSize::Integer, reducedSize::Integer, numPartitions::Integer)
     partitionList = []
-    for index in 1:numPartitions:
+    for index in 1:numPartitions
         partitionAccepted = false
-        while !partitionAccepted:
+        while !partitionAccepted
             partition = Dict{Integer, Integer}
             # assigns each node in the original nodes to a new node in the reduced nodes
             labels = [1:reducedSize for i=1:originalSize]
-            for node in 1:originalSize:
+            for node in 1:originalSize
                 partition[node] = labels[node]
             end
             # check that each partition is nonempty
-            if size(Set(labels),1) == reducedSize:
+            if size(Set(labels),1) == reducedSize
                 partitionAccepted = True
             end
         end
@@ -31,64 +32,74 @@ function spectralClustering(A::MatrixNetwork, reducedSize::Integer)
     labels = kmeans(X.vectors[:,1:reducedSize], reducedSize, init=:kmpp)[1]
     # return the partition
     return Dict{Integer, Integer}[i => labels[i] for i in 1:size(A,1)]
+end
 
-def mEEP(A, reducedSize):
+function mEEP(A::MatrixNetwork, reducedSize::Integer)
     return print("under dev")
+end
 
 # works only for undirected networks as of now.
-def agglomerationReduction(A, reducedSize):
-    partition = dict()
-    n = np.size(A, axis=0)
-    nodeIds = range(n)
-    for nodeId in nodeIds:
+function agglomerationReduction(A::MatrixNetwork, reducedSize::Integer)
+    partition = Dict{Integer, Integer}()
+    n = size(A, 1)
+    nodeIds = 1:n
+    # populate the partition
+    for nodeId in nodeIds
         partition[nodeId] = nodeId
+    end
 
-    k = np.sum(A, axis=0)
-    m = np.sum(k)
-    Q = -np.sum(np.power(k, 2))/m**2
-    while len(set(partition.values())) > reducedSize:
-        partition, Q = greedyMerge(A, Q, partition, k)
-        print(Q)
-    cleanedPartition = dict()
-    for node, group in partition.items():
-        try:
+    while size(Set(values(partition),1)) > reducedSize
+        partition, Q = greedyMerge(A, partition)
+    end
+    cleanedPartition = Dict{Integer, Integer}()
+    for (node, group) in partition:
+        try
             partition[node] = cleanedPartition[group]
-        except:
-            try:
+        catch
+            try
                 cleanedPartition[group] = max(cleanedPartition.values()) + 1
-            except:
+            catch
                 cleanedPartition[group] = 0
             partition[node] = cleanedPartition[group]
+            end
+        end
+    end
     return partition
+end
 
-def greedyMerge(A, Q, partition, k):
+function greedyMerge(A::MatrixNetwork, partition::Dict)
+    k = sum(A, dims=1)
+    Q = -sum(k.^2)/m^2
+    m = sum(k)
     # iterate over unigue groups
-    m = np.sum(k)
-    groupIds = list(set(partition.values()))
-    numGroups = len(groupIds)
-    maxQ = -math.inf
-    for groupIndex1 in range(numGroups):
-        for groupIndex2 in range(groupIndex1):
+    groupIds = collect(Set(values(partition)))
+    numGroups = size(groupIds,1)
+    maxQ = -Inf
+    for groupIndex1 in 1:numGroups
+        for groupIndex2 in 1:groupIndex1
             groupId1 = groupIds[groupIndex1]
             groupId2 = groupIds[groupIndex2]
 
-            group1 = [nodeId for nodeId, group in partition.items() if group == groupId1]
-            group2 = [nodeId for nodeId, group in partition.items() if group == groupId2]
+            group1 = [nodeId for (nodeId, group) in partition if group == groupId1]
+            group2 = [nodeId for (nodeId, group) in partition if group == groupId2]
 
             eUV = 0
             aU = 0
             aV = 0
-            for i in group1:
-                for j in group2:
-                    try:
-                        eUV += A[i,j]/m # because directed edge list
-                    except:
-                        pass
+            for i in group1
+                for j in group2
+                    try
+                        eUV += sparse(A)[i,j]/m # because directed edge list
+                    catch
+                    end
+                end
             for i in group1:
                 aU += k[i]/m
+            end
 
             for i in group2:
                 aV += k[i]/m
+            end
 
             newQ = Q + 2*(eUV - aU*aV)
 
@@ -96,23 +107,29 @@ def greedyMerge(A, Q, partition, k):
                 maxQ = newQ
                 maxGroupId1 = groupId1
                 maxGroupId2 = groupId2
-    newPartition = dict(partition)
-    for node in list(newPartition.keys()):
-        if newPartition[node] == maxGroupId2:
+            end
+        end
+    end
+    newPartition = copy(partition)
+    for node in collect(keys(newPartition))
+        if newPartition[node] == maxGroupId2
             newPartition[node] = maxGroupId1
+        end
+    end
     return newPartition, maxQ
+end
 
 # WARNING: not super efficient at the moment!
-def exhaustivePartition(n):
-    '''
+function exhaustivePartition(n::Integer)
+    #=
     input: number of nodes n
     output: dictionary of all possible partitions
-    '''
+    =#
     allPartitions = {}
-    nodeIds = list(range(n))
+    nodeIds = [1:n]
 
     # compute all integer partitions
-    for index, part in enumerate(partitionNodes(nodeIds),1):
+    for (index, part) in enumerate(partitionNodes(nodeIds),1):
         # print(index, part)
 
         partition = dict()
@@ -124,22 +141,22 @@ def exhaustivePartition(n):
 
     return allPartitions
 
-def partitionNodes(nodeIds):
-    '''
+@resumable function partitionNodes(nodeIds::Vector{Integer})
+    #=
     input:  list of nodeIds
     output: list of all possible partitions
-    '''
-    if len(nodeIds) == 1:
-        yield [ nodeIds ]
+    =#
+    if size(nodeIds,1) == 1
+        @yield nodeIds
         return
 
-    first = nodeIds[0]
-    for smaller in partitionNodes(nodeIds[1:]):
+    first = nodeIds[1]
+    for smaller in partitionNodes(nodeIds[2:end]):
         # insert first in each of the subpartition's subsets
         for k, subset in enumerate(smaller):
-            yield smaller[:k] + [[ first ] + subset] + smaller[k+1:]
+            @yield smaller[1:k] + [[ first ] + subset] + smaller[k+1:]
         # put first in its own subset
-        yield [[ first ]] + smaller
+        @yield [[ first ]] + smaller
 
 def kPartition(n, k):
     '''
