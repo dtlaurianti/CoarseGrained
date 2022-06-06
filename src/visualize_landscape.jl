c6d8d6da087add0619793fa1f747449f276d6147
JohnETobin <johntobin788@gmail.com>
2022-06-06 13:31:24 -0500

    Make visualize_landscape more Julian

--- src/visualize_landscape.jl
+++ src/visualize_landscape.jl
@@ -30,15 +30,15 @@ include("SimulateDynamics.jl")
 #   based distance. Journal of Multivariate Analysis, 98,
 #   873-895. doi:10.1016/j.jmva.2006.11.013
 # https://gist.github.com/jwcarr/626cbc80e0006b526688
-function variation_of_information(X, Y):
+function variation_of_information(X, Y)
   n = float(sum([size(x,1) for x in X]))
   σ = 0.0
-  for x in X:
+  for x in X
     p = size(x,1) / n
-    for y in Y:
+    for y in Y
       q = size(y,1) / n
       r = size(Set(x) & Set(y)) / n
-      if r > 0.0:
+      if r > 0.0
         σ += r * (log(r / p, 2) + log(r / q, 2))
       end
     end
@@ -50,16 +50,19 @@ end
 #
 #Input: A list of dictionary partitions
 #Output: An array of partitions
-function dict_to_array(dictionary::Array{Dict,1}):
+function dict_to_array(dictionary::Array{Dict,1})
     Arr = []
-    for i in range(size(dictionary,1)):
+    for i in range(size(dictionary,1))
         s = findmax(dictionary[i])[1]
         x = [[] for i in 1:(s+1)]
-        for (key, value) in dictionary:
+        for (key, value) in dictionary
             v = dictionary[i][key]
             append!(x[v], key)
+        end
         append!(Arr, x)
-    return Arr
+    end
+  return Arr
+end
 
 #Function surfaceplots
 #
@@ -76,19 +79,21 @@ function dict_to_array(dictionary::Array{Dict,1}):
 #  P = RN.generateRandomPartitions(10,7,100)
 #  surfaceplots(P,A)
 #
-def surfaceplots(partitions, A, save_to_string=None):
+function surfaceplots(partitions, A, save_to_string=None)
     #convert dictionary to an array
     Arr = dict_to_array(partitions)
 
     #calculate distance matrix
     num_par = len(partitions)
     D = [[0 for i in range(num_par)] for j in range(num_par)]
-    for i in range(num_par):
-        for j in range(num_par):
+    for i in range(num_par)
+        for j in range(num_par)
           D[i][j] = variation_of_information(Arr[i],Arr[j])
+        end
+    end
 
     #calculate MDS on disimilarity matrix
-    embedding = MDS(n_components=2, dissimilarity='precomputed')
+    embedding = MDS(n_components=2, dissimilarity="precomputed")
     X_transformed = embedding.fit_transform(D)
 
     #Format data
@@ -96,24 +101,27 @@ def surfaceplots(partitions, A, save_to_string=None):
     y = X_transformed[:,1]
 
     #Calculate z dimension
-    z = np.zeros(num_par)
-    for i in range(num_par):
-      loss = EE.getLoss(A,partitions[i],np.ones(10), SimulateDynamics.linear_model, 10, 0.01, {"epsilon":-0.3})
+    z = zeros(num_par)
+    for i in range(num_par)
+      loss = EvaluateError.getLoss(A, partitions[i], ones(10), SimulateDynamics.linear_model, 10, 0.01, {"epsilon":-0.3})
       z[i] = loss
+    end
 
     #Save vector data if we want to smooth it later
-    if save_to_string is not None:
+    if save_to_string != None
       df = pd.DataFrame({'x' : x, 'y' : y, 'z' : z})
       loc = "data/visualization_data/" + save_to_string + ".csv"
       df.to_csv(loc, index = False)
+    end
 
     #Plot surface
     triang = mtri.Triangulation(x,y)
     fig = plt.figure()
-    ax = fig.add_subplot(1,2,2,projection='3d')
+    ax = fig.add_subplot(1,2,2,projection="3d")
     surf = ax.plot_trisurf(triang,z,cmap=plt.cm.CMRmap,antialiased=True)
 
     plt.show()
+end
 
 #Function surfaceplots2
 #
@@ -130,22 +138,24 @@ def surfaceplots(partitions, A, save_to_string=None):
 #  P = RN.generateRandomPartitions(10,7,100)
 #  surfaceplots(P,A)
 #
-def surfaceplots2(parameters, numSamples, save_to_string=None):
+function surfaceplots2(parameters, numSamples, save_to_string=None)
     #Generate partitions
-    partitions = RN.generateRandomPartitions( parameters["originalSize"], parameters["reducedSize"], numSamples)
+    partitions = ReduceNetwork.generateRandomPartitions( parameters["originalSize"], parameters["reducedSize"], numSamples)
 
     #convert dictionary to an array
     Arr = dict_to_array(partitions)
 
     #calculate distance matrix
-    num_par = len(partitions)
+    num_par = length(partitions)
     D = [[0 for i in range(num_par)] for j in range(num_par)]
-    for i in range(num_par):
-        for j in range(num_par):
+    for i in range(num_par)
+        for j in range(num_par)
           D[i][j] = variation_of_information(Arr[i],Arr[j])
+        end
+    end
 
     #calculate MDS on disimilarity matrix
-    embedding = MDS(n_components=2, dissimilarity='precomputed')
+    embedding = MDS(n_components=2, dissimilarity="precomputed")
     X_transformed = embedding.fit_transform(D)
 
     #Format data
@@ -153,57 +163,64 @@ def surfaceplots2(parameters, numSamples, save_to_string=None):
     y = X_transformed[:,1]
 
     #Generate graph (must be connected)
-    isAccepted = False
-    while not isAccepted:
-        if parameters["graphType"] == "random":
+    isAccepted = false
+    while !isAccepted
+        if parameters["graphType"] == "random"
             G = nx.fast_gnp_random_graph(parameters["originalSize"], parameters["graphArgs"]["p"])
-        elif parameters["graphType"] == "cycle":
+        elseif parameters["graphType"] == "cycle"
             G = GenerateGraphs.cycle_graph(parameters["originalSize"], directed=False)
-        if nx.is_connected(G):
+        end
+        if nx.is_connected(G)
             isAccepted = True
+        end
+    end
 
     #Convert to adj matrix
     A = nx.to_numpy_array(G)
 
     # select model
-    if parameters["modelType"] == "linear_model":
+    if parameters["modelType"] == "linear_model"
         modelFunc = SimulateDynamics.linear_model
-    elif parameters["modelType"] == "SIS":
+    elseif parameters["modelType"] == "SIS"
         modelFunc = SimulateDynamics.SIS_model
-    elif parameters["modelType"] == "SI":
+    elseif parameters["modelType"] == "SI"
         modelFunc = SimulateDynamics.SI_model
-    elif parameters["modelType"] == "kuramoto_model":
+    elseif parameters["modelType"] == "kuramoto_model"
         modelFunc = SimulateDynamics.kuramoto_model
+    end
 
     tmax, tinc = parameters["tmax"], parameters["tinc"]
     modelArgs  = parameters["modelArgs"]
-    initial_condition = np.ones(10)
+    initial_condition = ones(10)
 
     #Calculate z dimension
-    z = np.zeros(num_par)
-    for i in range(num_par):
+    z = zeros(num_par)
+    for i in range(num_par)
       loss = EE.getLoss(A, partitions[i], initial_condition, modelFunc, tmax, tinc, modelArgs)
       z[i] = loss
+    end
 
     #Save vector data if we want to smooth it later
-    if save_to_string is not None:
+    if save_to_string != None
       df = pd.DataFrame({'x' : x, 'y' : y, 'z' : z})
       loc = "data/visualization_data/" + save_to_string + ".csv"
       df.to_csv(loc, index = False)
+    end
 
     #Plot surface
     triang = mtri.Triangulation(x,y)
     fig = plt.figure()
-    ax = fig.add_subplot(1,2,2,projection='3d')
+    ax = fig.add_subplot(1,2,2,projection="3d")
     surf = ax.plot_trisurf(triang,z,cmap=plt.cm.CMRmap,antialiased=True)
 
     plt.show()
+end
 
 #function plot_smoothed_surface
 #
 #Input: A csv file which has been smoothed by the R function
 #Output: No output, just a plot
-def plot_smoothed_surface(data):
+function plot_smoothed_surface(data)
   #Grab data from file
   data = pd.read_csv(data)
   x = data['x']
@@ -213,17 +230,18 @@ def plot_smoothed_surface(data):
   #Plot surface
   triang = mtri.Triangulation(x,y)
   fig = plt.figure()
-  ax = fig.add_subplot(1,2,2,projection='3d')
+  ax = fig.add_subplot(1,2,2,projection="3d")
   surf = ax.plot_trisurf(triang,z,cmap=plt.cm.CMRmap,antialiased=True)
 
   plt.show()
+end
 
 
 # function subplot_smoothed_surface
 # a subplot version of plot_smoothed_surface
 # Input: A csv file which has been smoothed by the R function, (optional) ax
 # Output: A subplot
-def subplot_smoothed_surface(data, fig, ax=None):
+function subplot_smoothed_surface(data, fig, ax=None)
   #Grab data from file
   data = pd.read_csv(data)
   x = data['x']
@@ -232,13 +250,14 @@ def subplot_smoothed_surface(data, fig, ax=None):
 
   #Plot surface
   triang = mtri.Triangulation(x,y)
-  if ax is not None:
-      ax = fig.add_subplot(1,2,ax,projection='3d')
-  else:
-      ax = fig.add_subplot(1,2,1,projection='3d')
+  if ax != None
+      ax = fig.add_subplot(1,2,ax,projection="3d")
+  else
+      ax = fig.add_subplot(1,2,1,projection="3d")
+  end
   surf = ax.plot_trisurf(triang,z,cmap=plt.cm.CMRmap,antialiased=True)
-
   return surf
+end
 
 #Test Examples
 #G = nx.fast_gnp_random_graph(10, 0.3)
