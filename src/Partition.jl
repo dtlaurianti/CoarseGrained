3da4c367375b2d8a4ab6a49dcbeb8d78c784f127
JohnETobin <johntobin788@gmail.com>
2022-06-07 15:40:25 -0500

    Converting Struct to Array

--- src/Partition.jl
+++ src/Partition.jl
@@ -1,6 +1,7 @@
 using SparseArrays
 using Clustering
 using ResumableFunctions
+using Parameters
 
 # generate numPartitions random partitions mapping a network of size originalSize
 # to a network of size reducedSize
--- src/SimulateDynamics.jl
+++ src/SimulateDynamics.jl
@@ -2,8 +2,8 @@ using MatrixNetworks
 using LinearAlgebra
 using DifferentialEquations
 using SparseArrays
-using Parameters
 
+#=
 # create a common Type for passing parameters
 Base.@kwdef struct Model_Parameters
   A::SparseMatrixCSC
@@ -15,45 +15,51 @@ Base.@kwdef struct Model_Parameters
   c::Number=1
   b::Number=0
 end
+=#
 
-function linear_model(du::Vector, u::Vector, p::Model_Parameters, t::Number)
-  du .= (p.A-I)*u
+function getModelParameters(A::SparseMatrixCSC;  β::Number=0, γ::Number=0, ω::Number=0, K::Number=1, d::Number=0.1, c::Number=1, b::Number=0)
+   Model_Parameters = [A, β, γ, ω, K, d, c, b]
+   return Model_Parameters
 end
 
-function SIS_model(du::Vector, u::Vector, p::Model_Parameters, t::Number)
-  du .= -p.γ.*u + p.β.*(ones(length(u))-u).*(p.A*u)
+function linear_model(du::Vector, u::Vector, p::Array, t::Number)
+  du .= (p[1]-I)*u
 end
 
-function SI_model(du::Vector, u::Vector, p::Model_Parameters, t::Number)
-  du .= p.β.*(ones(length(u))-u).*(p.A*u)
+function SIS_model(du::Vector, u::Vector, p::Array, t::Number)
+  du .= -p[3].*u + p[2].*(ones(length(u))-u).*(p[1]*u)
 end
 
-function kuramoto_model(du::Vector, u::Vector, p::Model_Parameters, t::Number)
-    n = size(p.A, 1)
-    dxdt = ones(n).*p.ω
+function SI_model(du::Vector, u::Vector, p::Array, t::Number)
+  du .= p[2].*(ones(length(u))-u).*(p[1]*u)
+end
+
+function kuramoto_model(du::Vector, u::Vector, p::Array, t::Number)
+    n = size(p[1], 1)
+    dxdt = ones(n).*p[4]
     for i in 1:n
         S = sin.(u[i].*ones(n).-(u))
-        dxdt .+= p.K.*(p.A[i,:].*(S))
+        dxdt .+= p[5].*(p[1][i,:].*(S))
     end
     return dxdt
 end
 
-function LotkaVolterra_model(du::Vector, u::Vector, p::Model_Parameters, t::Number)
-  n = size(p.A, 1)
-  return p.ω.*u + (u.*(p.A⋅u))
+function LotkaVolterra_model(du::Vector, u::Vector, p::Array, t::Number)
+  n = size(p[1], 1)
+  return p[4].*u + (u.*(p[1]⋅u))
 end
 
-function linear_opinions(du::Vector, u::Vector, p::Model_Parameters, t::Number)
+function linear_opinions(du::Vector, u::Vector, p::Array, t::Number)
   #=
   Simplest model of linear opinion dynamics; c = constant, for now
   =#
-  Dout = diag(sum(p.A, 1))            # compute outdegree matrix
-  Din  = diag(sum(p.A, 2))            # compute indegree matrix
-  L =  Dout + Din - p.A - transpose(p.A)  # compute graph laplacian
+  Dout = diag(sum(p[1], 1))            # compute outdegree matrix
+  Din  = diag(sum(p[1], 2))            # compute indegree matrix
+  L =  Dout + Din - p[1] - transpose(p[1])  # compute graph laplacian
   return -c.*L⋅x
 end
 
-function nonlinear_opinions(du::Vector, u::Vector, p::Model_Parameters, t::Number)
+function nonlinear_opinions(du::Vector, u::Vector, p::Array, t::Number)
   #=
   An instance of the model in https://arxiv.org/abs/2009.04332
   Parameters:
@@ -62,14 +68,14 @@ function nonlinear_opinions(du::Vector, u::Vector, p::Model_Parameters, t::Numbe
       b = bias
   For now, d, c, b are constants, but we can eventually vary by individual
   =#
-  n = size(p.A, 1)
-  return - p.d.*u + p.c.*tanh.(p.A⋅u) + p.b.*ones(n)
+  n = size(p[1], 1)
+  return - p[6].*u + p[7].*tanh.(p[1]⋅u) + p[8].*ones(n)
 end
 
 function simulateODEonGraph(A::MatrixNetwork, initial_condition::Vector; dynamical_function::Function=linear_model, tmax::Number=10, dt::Number=0.01, function_args...)
   tspan = (0, tmax)
   # splat the MatrixNetwork and additional parameters into one parameters Tuple
-  p = Model_Parameters(A=sparse(A); function_args...)
+  p = getModelParameters(sparse(A); function_args...)
   prob = ODEProblem(dynamical_function, initial_condition, tspan, p)
   sol = solve(prob, saveat=dt)
   # time_series = solve_ivp(t, x -> dynamical_function(t, x, A, function_args), (0, tmax), initial_condition, t_eval=t)
