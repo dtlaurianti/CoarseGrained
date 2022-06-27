0036f4c8c74d3bc6259c0184a4ef697de3a94f65
dtlaurianti <dtlaurianti@gmail.com>
2022-06-27 14:39:16 -0500

    Create ServerStartup.jl

---
+++ src/ServerStartup.jl
@@ -0,0 +1,21 @@
+using Distributed
+const numCores = 104
+if (nworkers() != numCores)
+    rmprocs(procs()[2:end])
+    addprocs(numCores)
+end
+@everywhere begin
+    using Test
+    using BenchmarkTools
+    include("../src/SimulateDynamics.jl")
+    include("../src/GenerateGraphs.jl")
+    include("../src/Partition.jl")
+    include("../src/ReduceNetwork.jl")
+    include("../src/EvaluateError.jl")
+    include("../src/visualize_landscape.jl")
+    include("../src/LocalSearch.jl")
+end
+
+using Plots
+using GraphRecipes
+using Dates
